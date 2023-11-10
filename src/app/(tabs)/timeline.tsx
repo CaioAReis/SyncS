a9e2420a8f0813011e0c82760e6a7c6fd7ffd187
CaioAReis <caio.almeida16.ca@gmail.com>
2023-11-09 21:35:40 -0300

    Iniciando a tela de timeline

--- src/app/(tabs)/timeline.tsx
+++ src/app/(tabs)/timeline.tsx
@@ -1,13 +1,141 @@
-import { View } from "react-native";
+import { SectionList, View } from "react-native";
 import { Text } from "../../components";
+import { useAppTheme } from "../../theme";
+
+interface TimelineItem {
+  _id: string,
+  year: number,
+  data: TimelinePeriod[],
+}
+
+interface TimelinePeriod {
+  _id: string,
+  body: string,
+  title: string,
+  galery?: string[],
+  when: { day: number, month: string },
+}
+
+const DATA: TimelineItem[] = [
+
+  {
+    _id: "1",
+    year: 2020,
+    data: [
+      {
+        _id: "1",
+        galery: [],
+        when: { day: 1, month: "Agosto" },
+        title: "1º Semana Acadêmica IFS - Campus Lagarto",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
+      },
+
+      {
+        _id: "2",
+        galery: [],
+        when: { day: 1, month: "Agosto" },
+        title: "2º Semana Acadêmica IFS - Campus Lagarto",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
+      },
+
+      {
+        _id: "3",
+        galery: [],
+        when: { day: 1, month: "Agosto" },
+        title: "3º Semana Acadêmica IFS - Campus Lagarto",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
+      },
+    ]
+  },
+
+  {
+    _id: "1",
+    year: 2021,
+    data: [
+      {
+        _id: "1",
+        galery: [],
+        when: { day: 1, month: "Agosto" },
+        title: "1º Semana Acadêmica IFS - Campus Lagarto",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
+      },
+
+      {
+        _id: "2",
+        galery: [],
+        when: { day: 1, month: "Agosto" },
+        title: "2º Semana Acadêmica IFS - Campus Lagarto",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
+      },
+
+      {
+        _id: "3",
+        galery: [],
+        when: { day: 1, month: "Agosto" },
+        title: "3º Semana Acadêmica IFS - Campus Lagarto",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
+      },
+    ]
+  },
+
+  {
+    _id: "1",
+    year: 2022,
+    data: [
+      {
+        _id: "1",
+        galery: [],
+        when: { day: 1, month: "Agosto" },
+        title: "1º Semana Acadêmica IFS - Campus Lagarto",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
+      },
+
+      {
+        _id: "2",
+        galery: [],
+        when: { day: 1, month: "Agosto" },
+        title: "2º Semana Acadêmica IFS - Campus Lagarto",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
+      },
+    ]
+  },
+
+];
 
 export default function Timeline() {
+  const { colors } = useAppTheme();
 
   return (
-    <View style={{ flex: 1, backgroundColor: "lightgreen", alignItems: "center", justifyContent: "center" }}>
-      
-      <Text fw="BOLD">TELA DE TIMELINE</Text>
-      
+    <View style={{ flex: 1, backgroundColor: colors.background }}>
+
+      <SectionList
+        sections={DATA}
+        keyExtractor={item => item?._id}
+        ListHeaderComponent={
+          <View style={{ marginVertical: 20 }}>
+            <Text fw="BOLD" ta="center">TIMELINE IFS</Text>
+          </View>
+        }
+
+        //  Title timeline
+        renderSectionHeader={({ section }) => (
+          <Text
+            fs={28}
+            fw="BOLD"
+            style={{ marginHorizontal: 20, marginVertical: 10 }}
+          >
+            {section?.year}
+          </Text>
+        )}
+
+        //  Card Timeline
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
+
     </View>
   );
 }
