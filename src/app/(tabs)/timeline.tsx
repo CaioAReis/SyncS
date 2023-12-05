f07255e622d3a9fa0683d45c9ea109caea9b5a55
CaioAReis <caio.almeida16.ca@gmail.com>
2023-12-04 21:30:32 -0300

    Integrando o timeline

--- src/app/(tabs)/timeline.tsx
+++ src/app/(tabs)/timeline.tsx
@@ -1,130 +1,59 @@
-import { SectionList, View } from "react-native";
+import { useLayoutEffect, useState } from "react";
+import { Image, SectionList, View, useWindowDimensions } from "react-native";
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
 
 import { useAppTheme } from "../../theme";
 import { TimelineItem } from "../../types";
+import { db } from "../../services/firebaseConfig";
 import { Text, TimelineCard } from "../../components";
-
-const DATA: TimelineItem[] = [
-
-  {
-    _id: "1",
-    year: 2020,
-    data: [
-      {
-        _id: "1",
-        gallery: [
-          { code: "001", image: "https://humulos.com/digimon/images/art/metalgaruru.jpg" },
-          { code: "002", image: "https://humulos.com/digimon/images/art/metalgrey_am.jpg" },
-          { code: "003", image: "https://humulos.com/digimon/images/art/weregaruru_s.jpg" },
-        ],
-        when: { day: 1, month: "Agosto" },
-        title: "1º Semana Acadêmica IFS - Campus Lagarto",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
-      },
-
-      {
-        _id: "2",
-        gallery: [
-          { code: "004", image: "https://humulos.com/digimon/images/art/chaosdra.jpg" },
-          { code: "005", image: "https://humulos.com/digimon/images/art/ryuda.jpg" },
-          { code: "006", image: "https://humulos.com/digimon/images/art/ouryu.jpg" },
-          { code: "007", image: "https://humulos.com/digimon/images/art/duke_x.jpg" },
-          { code: "008", image: "https://humulos.com/digimon/images/art/galgo.jpg" },
-          { code: "009", image: "https://humulos.com/digimon/images/art/cherubi_vice.jpg" },
-          { code: "010", image: "https://humulos.com/digimon/images/art/tao.jpg" },
-          { code: "011", image: "https://humulos.com/digimon/images/art/kaisergrey.jpg" },
-        ],
-        when: { day: 1, month: "Agosto" },
-        title: "2º Semana Acadêmica IFS - Campus Lagarto",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
-      },
-
-      {
-        _id: "3",
-        gallery: [],
-        when: { day: 1, month: "Agosto" },
-        title: "3º Semana Acadêmica IFS - Campus Lagarto",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
-      },
-    ]
-  },
-
-  {
-    _id: "1",
-    year: 2021,
-    data: [
-      {
-        _id: "1",
-        gallery: [],
-        when: { day: 1, month: "Agosto" },
-        title: "1º Semana Acadêmica IFS - Campus Lagarto",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
-      },
-
-      {
-        _id: "2",
-        gallery: [],
-        when: { day: 1, month: "Agosto" },
-        title: "2º Semana Acadêmica IFS - Campus Lagarto",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
-      },
-
-      {
-        _id: "3",
-        gallery: [],
-        when: { day: 1, month: "Agosto" },
-        title: "3º Semana Acadêmica IFS - Campus Lagarto",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
-      },
-    ]
-  },
-
-  {
-    _id: "1",
-    year: 2022,
-    data: [
-      {
-        _id: "1",
-        gallery: [],
-        when: { day: 1, month: "Agosto" },
-        title: "1º Semana Acadêmica IFS - Campus Lagarto",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
-      },
-
-      {
-        _id: "2",
-        gallery: [],
-        when: { day: 1, month: "Agosto" },
-        title: "2º Semana Acadêmica IFS - Campus Lagarto",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ante eros. Maecenas efficitur arcu pulvinar dui accumsan condimentum.",
-      },
-    ]
-  },
-
-];
 
 export default function Timeline() {
   const { colors } = useAppTheme();
+  // const [lastPeiod, setLastPeriod] = useState();
+  const { width, height } = useWindowDimensions();
+  const [timeline, setTimeline] = useState<TimelineItem[]>([]);
+
+  const getTimeline = async () => {
+    const firstPage = query(collection(db, "timeline"), orderBy("year", "desc"), limit(2));
+    await getDocs(firstPage).then(result => {
+      setTimeline(result.docs.map(doc => doc.data() as TimelineItem));
+      // setLastPeriod(result.docs[result.docs.length - 1]);
+    });
+  };
+
+  // const getNextTimeline = async () => {
+  //   const nextPage = query(collection(db, "timeline"), orderBy("year", "desc"), startAfter(lastVisible), limit(2));
+  //   await getDocs(nextPage).then(result => {
+  //     setTimeline(result.docs.map(doc => doc.data() as TimelineItem));
+  //     setLastPeriod(result.docs[result.docs.length - 1]);
+  //   });
+  // };
+
+  useLayoutEffect(() => {
+    getTimeline();
+  }, []);
 
   return (
     <View style={{ flex: 1, backgroundColor: colors.background }}>
       <SectionList
-        sections={DATA}
+        sections={timeline}
         keyExtractor={item => item?._id}
-        ListHeaderComponent={
-          <View style={{ marginVertical: 20 }}>
-            <Text fw="BOLD" ta="center">TIMELINE IFS</Text>
+        ListEmptyComponent={
+          <View style={{ alignItems: "center", height: height - 150, justifyContent: "center" }}>
+            <Image
+              style={{ width: width / 1.5, height: width / 1.5 }}
+              source={require("../../../assets/images/Request.png")}
+            />
+            <Text ta="center" fs={16}>
+              {"No momento não exitem atualizações \nem nossa timeline, volte mais tarde."}
+            </Text>
           </View>
         }
 
         //  Title timeline
         renderSectionHeader={({ section }) => (
           <View style={{ flexDirection: "row", alignItems: "center", backgroundColor: colors.background }}>
-            <Text
-              fs={28}
-              fw="BOLD"
-              style={{ marginHorizontal: 20, marginVertical: 10 }}
-            >
+            <Text fs={28} fw="BOLD" style={{ marginHorizontal: 20, marginVertical: 10 }}>
               {section?.year}
             </Text>
 
@@ -142,7 +71,6 @@ export default function Timeline() {
 
         ListFooterComponent={<View style={{ marginVertical: 30 }} />}
       />
-
     </View>
   );
 }
